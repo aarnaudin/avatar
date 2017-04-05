52c2e8f253040b9a6809610b837ce014d2ededa4
aarnaudin <ant.arnaudin@gmail.com>
2017-04-05 17:30:27 +0200

    Modification controleur pour ajout avatar

---
+++ app/Http/Controllers/AvatarController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AvatarController extends Controller
+{
+    public function showAvatarsList(){
+        $mails = Mail::with('users')->where('user_id','=', Auth::id())->get();
+        return view('home')->with(array('mails'=>$mails));
+    }
+
+
+    public function addAvatar(Request $request)
+    {
+        $rules = array(
+            'avatar' => 'required|mimes:jpeg,png',
+            'mail' => 'required|email',
+        );
+
+        $frMessages = [
+            'mail.required' => 'L\'adresse mail n\'est pas renseignée !',
+            'mail.email' => 'L\'adresse mail n\'est pas de type mail (Exemple : exemple@exemple.fr) !',
+            'avatar.required' => 'Aucun avatar n\'a été téléchargé !',
+            'avatar.mimes' => 'Le format du fichier doit être .jpeg, .jpg ou .png !',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $frMessages);
+
+        if ($validator->passes()){
+            $mail = new Mail;
+            $mail -> adress = $request -> mail;
+            $mail -> user_id = Auth::id();
+            $mail -> url_avatar = "";
+            $mail->save();
+
+            $avatarName = $mail->id.'.'.
+                $request->avatar->getClientOriginalName();
+
+
+            $request->file('avatar')->move(
+                base_path().'/public/assets/avatars/', $avatarName
+            );
+
+            $urlAvatar = asset('assets/avatars/'.$avatarName);
+            $mail-> url_avatar = $urlAvatar;
+            $mail->save();
+
+            return redirect('/');
+        }
+
+        elseif ($validator->fails()){
+          return redirect('/')->withErrors($validator);
+        }
+    }
+
+
+
+}
