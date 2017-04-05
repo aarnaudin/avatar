76479254c44e65c9721e79ce71d03e73949e95c3
emlerock <emlerock@yahoo.fr>
2017-04-05 17:34:03 +0200

    Update api files

--- config/api.php
+++ config/api.php
@@ -4,8 +4,8 @@ return [
 
     'api' =>[
         'api_version'=> 'V1',
-    'avatar_size'=>'100*100',
-    'avatar_dafault_size'=>'50x50',
-    'format_supported'=>'jpg',
+        'avatar_size'=>'100*100,50x50',
+        'avatar_default_size'=>'50x50',
+        'format_supported'=>'jpg',
     ]
     ];
