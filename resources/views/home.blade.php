98ac1b29f981d8b590cf63a9f12bfda95bc50c99
rduret <remi.duret64@gmail.com>
2017-04-05 10:10:26 +0200

    Création des controllers

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
                     @foreach ($mails as $m)
                         {{ $m -> adress }}
                         <img src="{{ $m -> url_avatar}}"/>
-                    @endforeach
+                    @endforeachs
                 </div>
             </div>
         </div>
