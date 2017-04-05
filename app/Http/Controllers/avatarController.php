e890f82768af2ea7e0fe652b45fa4059d5db3075
rduret <remi.duret64@gmail.com>
2017-04-05 10:15:10 +0200

    Version fonctionnelle 05/04

--- app/Http/Controllers/avatarController.php
+++ app/Http/Controllers/avatarController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Mail;
+use Illuminate\Support\Facades\Auth;
 
 class avatarController extends Controller
 {
-    public function showAvatarsList($id){
-        $mails = Mail::with('users')->where('user_id','=',$id)->get();
-        return view('home', ['mails' => $mails]);
+    public function showAvatarsList(){
+        $mails = Mail::with('users')->where('user_id','=', Auth::id())->get();
+        return view('home')->with(array('mails'=>$mails));
     }
 }
